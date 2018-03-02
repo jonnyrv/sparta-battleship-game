879bd7f963f470c4eb4893c47fc960a50a6743f4
Tech-A04 <tech-a04@MacBook-Air-7.local>
2018-03-02 12:07:39 +0000

    Added audio and cleaner code

--- js/battleships.js
+++ js/battleships.js
@@ -19,42 +19,10 @@ $(function(){
     var damageShipPosition2 = [];
     var damageShipPosition3 = [];
     var winCond = false;
-    
-
-
-    
-
+ 
     generateNumberArray(shipLocation1, 6, 0 );
     generateNumberArray(shipLocation2, 14, 8);
     generateNumberArray(shipLocation3, 22, 16);
-    // var generateRandom = Math.floor(Math.random() * 5);
-    // console.log(generateRandom);
-    // if(generateRandom >= 0){
-    //     console.log("abc");
-    //     shipLocation4.push(generateRandom);
-    //     console.log(shipLocation4);
-    //     for (let i = 0; i < 2; i++) {
-    //         generateRandom++;
-    //         console.log(generateRandom);
-    //         shipLocation4.push(generateRandom);
-    //         console.log(shipLocation4);
-    //     }
-    // }
-
-    // var generateRandom = Math.floor(Math.random()*(13-5+1)+5);;
-    // console.log(generateRandom);
-    // if(generateRandom >= 0){
-    //     console.log("abc");
-    //     shipLocation5.push(generateRandom);
-    //     console.log(shipLocation5);
-    //     for (let i = 0; i < 2; i++) {
-    //         generateRandom++;
-    //         console.log(generateRandom);
-    //         shipLocation5.push(generateRandom);
-    //         console.log(shipLocation5);
-    //     }
-    // }
-
 
     function generateNumberArray(shiplocation, max, min){
         var generateRandom = Math.floor(Math.random() * (max-min+1) + min);
@@ -63,7 +31,6 @@ $(function(){
             for (let i = 0; i < 2; i++) {
                 generateRandom++;
                 shiplocation.push(generateRandom);
-                console.log(shiplocation);
             }
         }
     }
@@ -88,17 +55,8 @@ $(function(){
         $("[data-ship = " + torpedoHitLocation + "]").addClass("hit");
         //Condition for Losing the Game
         if(torpedoLeft == 0 && winCond == false){
-            // alert("You lost the game !!!");
             losemodal.style.display = "block";            
-            
         }
-
-  
-    //      var index = jQuery.inArray(torpedoHitLocations, shipLocation1);
-    //      alert(index);
-    //      if(index >= 0){
-    //          alert("HitBoi");
-    //  }
     });
 
     $("#resetBtn").click(function(){
@@ -123,14 +81,11 @@ $(function(){
     $.fn.chkShip = function(ship, message, hitLocation, attackShip){
         for (let i = 0; i < ship.length; i++) {
             if(ship[i] == hitLocation){
-                console.log("You hit !!!");
                 $(textAlert).html("You Hit the Enemy Ship !!!");
                 attackShip.push(hitLocation);
-                console.log(attackShip);
                 hitCount++;
                 $("[data-ship = " + hitLocation + "]").addClass("miss");
                 if(attackShip.length == 3){
-                    console.log("You destroyed a ship !!!");
                     $(textAlert).html("You destroyed an enemy ship ");
                 }
                 if(hitCount == 9){
@@ -138,62 +93,11 @@ $(function(){
                     $(textAlert).html(message);
                     winmodal.style.display = "block";
                 }
-                // var generateRandom = Math.floor(Math.random() * 13);
-                // console.log(generateRandom);
-                // if(generateRandom > 0){
-                //     console.log("abc");
-                //     var generateRandomList = [];
-                //     generateRandomList.push(generateRandom);
-                //     console.log(generateRandomList);
-                //     for (let i = 0; i < 2; i++) {
-                //         generateRandom++;
-                //         generateRandomList.push(generateRandom);
-                //         console.log(generateRandomList);
-                //     }
-                // }
                 break;
             }
         }
     }  
-
- 
-
-
-
-
-    // $.fn.generateRandomShips = function(shipLocation){
-    //     var generateRandom = Math.floor(Math.random() * 13);
-    //     console.log(generateRandom);
-    //     if(generateRandom > 0){
-    //         console.log("abc");
-    //         var shipLocation = [];
-    //         shipLocation.push(generateRandom);
-    //         console.log(shipLocation);
-    //         for (let i = 0; i < 2; i++) {
-    //             generateRandom++;
-    //             shipLocation.push(generateRandom);
-    //             console.log(shipLocation);
-    //         }
-    //     }
-    // }
-
-
 });
-
-
-
-
-
-
-        // for (let i = 0; i < shipLocation.length; i++) {
-        //     if(shipLocation[i] == torpedoHitLocation){
-        //         alert("You hit the fmm");
-        //     }
-        // }
-
-
-        // randomize number( randomize a number and then do a loop and increment it 2 times :D)
-
 
 // Get the modal
 var winmodal = document.getElementById('winModal');
